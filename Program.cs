b0af755290b1a4da8cf688d9a365f15e5dea4c6a
Daniel Guidry <dguidry@lcg.org>
2024-06-09 19:12:56 -0400

    Set up browser and open it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+
+// Modify this if you need to use a specific Edge profile.
+var profileName = "Default";
+
+Console.WriteLine("Opening Browser");
+
+// Set up browser and open it.
+var options = new EdgeOptions();
+
+// Set desired profile.
+var edgeUserDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Microsoft\\Edge\\User Data";
+options.AddArgument("user-data-dir=" + edgeUserDataPath);
+Console.WriteLine(edgeUserDataPath);
+options.AddArgument("profile-directory=" + profileName);
+options.AddArgument("--remote-debugging-port=9222");
+
+// Open browser.
+IWebDriver driver = new EdgeDriver(options);
