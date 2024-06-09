ab7a049e2f61b5bba4bcd585261f13b0e1ceba50
Daniel Guidry <dguidry@lcg.org>
2024-06-09 19:48:58 -0400

    Navigate to Bing and begin searching
    This will send the browser to bing.com, shuffle the list of search
    phrases, and then run through the top 30.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using OpenQA.Selenium.Edge;
 using OpenQA.Selenium;
+using BingRewardsAutoSearcher;
+using System.Security.Cryptography;
 
 // Modify this if you need to use a specific Edge profile.
 var profileName = "Default";
+
+// Modify this to change the number of searches that will occur. Should not exceed the number of available search phrases.
+var searchCount = 30;
 
 Console.WriteLine("Opening Browser");
 
@@ -18,3 +23,23 @@ options.AddArgument("--remote-debugging-port=9222");
 
 // Open browser.
 IWebDriver driver = new EdgeDriver(options);
+
+Console.WriteLine("Navigating to Bing");
+
+// Navigate to Bing
+driver.Navigate().GoToUrl("https://www.bing.com/");
+
+var searchPhrases = SearchPhrases.GetRandom(30);
+
+// Iterate through the number of shuffled search phrases specified above.
+for (int i = 0; i < searchCount; i++)
+{
+    // Locate the search box, input the search phrase, and submit.
+    var searchBox = driver.FindElement(By.Id("sb_form_q"));
+
+    searchBox.Clear();
+    searchBox.SendKeys(searchPhrases[i]);
+    searchBox.SendKeys(Keys.Enter);
+
+    Thread.Sleep(6000);
+}
